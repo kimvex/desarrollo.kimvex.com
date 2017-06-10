7aac74d2dd9ffafccbda12e13ece088eff8d47d9
AlexxRuff <alexxruff@yahoo.com>
2017-06-10 12:59:57 -0500

    se compusieron errores en contácto

--- js/contacto.js
+++ js/contacto.js
@@ -3,15 +3,23 @@ document.querySelector('#form-contact').addEventListener('submit', function(e){
 	var nombre = document.querySelector('#nombre').value;
 	var email = document.querySelector('#email').value;
 	var phone = document.querySelector('#phone').value;
+	var respuesta = document.getElementById('#resp-env');
 
 	$.ajax({
 		url: 'https://cursos-kimvex.herokuapp.com/envio',
 		method: 'POST',
-		data: JSON.stringify({
+		data: {
 			nombre: nombre,
 			email: email,
 			phone: phone
-		})
+		},
+		success: function(data){
+			if(data.dato){
+				respuesta.innerHTML = 'Se han enviado los datos de pago a tu correo.';
+			}else{
+				respuesta.innerHTML = 'Ha sucedido un error, intentalo de nuevo.';
+			}
+		}
 	});
 
 	e.preventDefault();
